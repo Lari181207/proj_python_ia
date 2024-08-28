06d0e5a098cd31b23798d506dc1e57e6a0a3b595
Larissa Lima <larissalimadossantos692@gmail.com>
2024-08-28 18:55:27 -0300

    aula de revisao

---
+++ aula 03/para2.py
@@ -0,0 +1,21 @@
+alunos = [
+"Larissa",
+"Evelin",
+"João",
+"Cauã",
+"Maria",
+"Daniel",
+"Giovanna",
+"Diogo",
+"Isadora",
+"Pedro"
+]
+alunos.sort()
+alunos.append("Evelin")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+    y=y+1
+
+
