6c216fae1988093a3360eff4bcc5082d05fd2546
Larissa Lima <larissalimadossantos692@gmail.com>
2024-08-28 20:20:18 -0300

    exercicio

---
+++ aula 03/Exercico/exercicio1.py
@@ -0,0 +1,19 @@
+Frutas = [
+"banana",
+"morango",
+"pitaya",
+"mirtilo",
+"kiwi",
+"maracuja",
+"framboesa",
+"laranja",
+"caqui",
+"melancia"
+]
+Frutas.sort()
+Frutas.append("pitaya")
+y = 1
+for x in Frutas:
+    print(y,"-",x)
+    y=y+1
+    
